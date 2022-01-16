3ade07adcd1ac7e239fbf6abed7bc1c70ef5a45e
Sunil D S <sunil.d.shiv@gmail.com>
2022-01-16 13:49:39 +0530

    Basic project structure commit with nav graph

---
+++ app/src/main/java/com/todo/notesapp/fragments/list/NotesListFragment.kt
@@ -0,0 +1,21 @@
+package com.todo.notesapp.fragments.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.todo.notesapp.R
+
+class NotesListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+    }
+
+
+}
